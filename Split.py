605a81e1eb7821c9136a0eaf2713e5cf35c9fc2e
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-05 23:39:21 +0700

    Update Split

--- Split.py
+++ Split.py
@@ -48,3 +48,39 @@ author_last_names = []
 for name in author_names:
   author_last_names.append(name.split()[-1])
 print(author_last_names)
+
+# This code is splitting the multi-line string at the newlines (\n) which exist at the end of each line and saving it to a new list called chorus_lines. Then it prints chorus_lines which will produce the output
+
+# ['And if you said, "This life ain\'t good enough."', 'I would give my world to lift you up', 'I could change my life to better suit your mood', "Because you're so smooth"]
+# The new list contains each line of the original string as it’s own smaller string. Also, notice that Python automatically escaped the ' character when it created the new list.
+smooth_chorus = \
+"""And if you said, "This life ain't good enough."
+I would give my world to lift you up
+I could change my life to better suit your mood
+Because you're so smooth"""
+
+chorus_lines = smooth_chorus.split('\n')
+
+print(chorus_lines)
+
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split("\n")
+print(spring_storm_lines)
